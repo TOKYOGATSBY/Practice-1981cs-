83388253b00db456c35aa19dd18f9a6219f66955
Grigorii Kolbeev <gold_grigorii@mail.ru>
2025-04-11 08:21:01 +0300

    version 1.0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+
+        long totalDiag = n * (n - 3) / 2;
+
+        long parallelCount;
+        if (n == 4)
+        {
+            parallelCount = 0;
+        }
+        else if (n == 5)
+        {
+            parallelCount = 0;
+        }
+        else if (n == 6)
+        {
+            parallelCount = 6;
+        }
+        else
+        {
+            parallelCount = totalDiag;
+        }
+
+        long perpCount;
+        if (n % 2 == 0)
+        {
+            perpCount = totalDiag;
+        }
+        else
+        {
+            perpCount = 0;
+        }
+
+        Console.WriteLine($"{parallelCount} {perpCount}");
+    }
+}
